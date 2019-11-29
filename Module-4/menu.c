8d9b6ec255a545ee4764aa1426b5ae965be1508c
dassimanuel000 <58192026+dassimanuel000@users.noreply.github.com>
2019-11-29 11:45:19 +0100

    Update menu.c

--- Module-4/menu.c
+++ Module-4/menu.c
@@ -3,7 +3,24 @@
 
 int main(){
 
+int choix;
 
+do{
+  printf("---------------------------MENU OPERATIONNEL----------------------------------");
+  printf("\n 1 : Pour Afficher les données dans l’ordre \n\n 2 : Pour Afficher les données en ordre croissant/décroissant (selon le temps, selon le pouls) \n\n 3 : Pour Rechercher et afficher les données pour un temps particulier \n\n 4 : Pour Afficher la moyenne de pouls dans une plage de temps donnée \n\n 5 : Pour Afficher le nombre de lignes de données actuellement en mémoire \n\n 6 : Pour Rechercher et afficher les max/min de pouls (avec le temps associé) \n");
+  scanf("%d", &choix);
+
+	switch(choix)
+	{
+	case 1:
+	printf("ee");
+	break;
+
+	case 2:
+	printf("wwwwww");
+	break;
+	}
+}while(choix == 0);
 
 return 0;
 
